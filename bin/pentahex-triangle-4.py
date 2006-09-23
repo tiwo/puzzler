8969f5f6e183685b1bf912ca05b1e44f8bfc1479
goodger <goodger@2299cb40-9015-0410-805f-d83012bcf56a>
2006-09-23 19:49:23 +0000

    added pentahex triangle & hexagram puzzles & solutions
    git-svn-id: https://svn.code.sf.net/p/puzzler/code/trunk/puzzler@153 2299cb40-9015-0410-805f-d83012bcf56a

---
+++ bin/pentahex-triangle-4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# $Id$
+
+""" solutions"""
+
+import puzzler.puzzles
+
+puzzler.run(puzzler.puzzles.PentahexTriangle4)
--- puzzler/puzzles.py
+++ puzzler/puzzles.py
@@ -3032,6 +3032,192 @@ class PentahexHexagon4(Pentahexes):
                     yield coordsys.Hexagonal2D((x, y))
 
 
+class PentahexTriangle1(Pentahexes):
+
+    height = 15
+    width = 15
+
+    def coordinates(self):
+        hole = set()
+        for y in range(3, 7):
+            for x in range(4, 8):
+                if x + y < 11:
+                    hole.add((x,y))
+        for y in range(self.height):
+            for x in range(self.width):
+                if x + y < self.width and (x,y) not in hole:
+                    yield coordsys.Hexagonal2D((x, y))
+
+
+class PentahexTriangle2(Pentahexes):
+
+    height = 15
+    width = 15
+
+    def coordinates(self):
+        hole = set()
+        for y in range(5, 9):
+            for x in range(3, 7):
+                if x + y < 12:
+                    hole.add((x,y))
+        for y in range(self.height):
+            for x in range(self.width):
+                if x + y < self.width and (x,y) not in hole:
+                    yield coordsys.Hexagonal2D((x, y))
+
+
+class PentahexTriangle3(Pentahexes):
+
+    height = 15
+    width = 15
+
+    def coordinates(self):
+        hole = set()
+        for y in range(7, 11):
+            for x in range(2, 6):
+                if x + y < 13:
+                    hole.add((x,y))
+        for y in range(self.height):
+            for x in range(self.width):
+                if x + y < self.width and (x,y) not in hole:
+                    yield coordsys.Hexagonal2D((x, y))
+
+
+class PentahexTriangle4(Pentahexes):
+
+    height = 15
+    width = 15
+
+    def coordinates(self):
+        hole = set()
+        for y in range(9, 13):
+            for x in range(1, 5):
+                if x + y < 14:
+                    hole.add((x,y))
+        for y in range(self.height):
+            for x in range(self.width):
+                if x + y < self.width and (x,y) not in hole:
+                    yield coordsys.Hexagonal2D((x, y))
+
+
+class PentahexTriangle5(Pentahexes):
+
+    height = 15
+    width = 15
+
+    def coordinates(self):
+        hole = set()
+        for y in range(1, 5):
+            for x in range(5, 9):
+                if x + y < 10:
+                    hole.add((x,y))
+        for y in range(self.height):
+            for x in range(self.width):
+                if x + y < self.width and (x,y) not in hole:
+                    yield coordsys.Hexagonal2D((x, y))
+
+
+class PentahexTriangle6(Pentahexes):
+
+    height = 15
+    width = 15
+
+    def coordinates(self):
+        hole = set()
+        for y in range(4, 7):
+            for x in range(3, 7):
+                if 7 < x + y < 12:
+                    hole.add((x,y))
+        for y in range(self.height):
+            for x in range(self.width):
+                if x + y < self.width and (x,y) not in hole:
+                    yield coordsys.Hexagonal2D((x, y))
+
+
+class PentahexHexagram1(Pentahexes):
+
+    height = 17
+    width = 17
+
+    def coordinates(self):
+        hole = self.hole_coordinates()
+        coords = set()
+        for y in range(4, 17):
+            for x in range(4, 17):
+                if x + y < 21 and (x,y) not in hole:
+                    yield coordsys.Hexagonal2D((x, y))
+                    coords.add((x,y))
+        for y in range(13):
+            for x in range(13):
+                if x + y > 11 and (x,y) not in hole and (x,y) not in coords:
+                    yield coordsys.Hexagonal2D((x, y))
+
+    def hole_coordinates(self):
+        return set(((7,7), (8,7), (9,7), (10,7),
+                    (7,8), (8,8), (9,8),
+                    (6,9), (7,9), (8,9), (9,9)))
+
+
+class PentahexHexagram2(PentahexHexagram1):
+
+    def hole_coordinates(self):
+        return set(((8,6), (10,6),
+                    (8,7), (9,7),
+                    (7,8), (8,8), (9,8),
+                    (7,9), (8,9),
+                    (6,10), (8,10)))
+
+
+class PentahexHexagram3(PentahexHexagram1):
+
+    def hole_coordinates(self):
+        return set(((9,6),
+                    (8,7), (9,7),
+                    (6,8), (7,8), (8,8), (9,8), (10,8),
+                    (7,9), (8,9),
+                    (7,10)))
+
+
+class PentahexHexagram4(PentahexHexagram1):
+
+    def hole_coordinates(self):
+        return set(((8,6), (9,6), (10,6),
+                    (8,7), (9,7),
+                    (8,8),
+                    (7,9), (8,9),
+                    (6,10), (7,10), (8,10)))
+
+
+class PentahexHexagram5(PentahexHexagram1):
+
+    def hole_coordinates(self):
+        return set(((9,6),
+                    (7,7), (8,7), (9,7), (10,7),
+                    (8,8),
+                    (6,9), (7,9), (8,9), (9,9),
+                    (7,10)))
+
+
+class PentahexHexagram6(PentahexHexagram1):
+
+    def hole_coordinates(self):
+        return set(((8,7), (9,7),
+                    (5,8), (6,8), (7,8), (8,8), (9,8), (10,8), (11,8),
+                    (7,9), (8,9)))
+
+
+class PentahexHexagram7(PentahexHexagram1):
+
+    def hole_coordinates(self):
+        return set(((9,5), (10,5),
+                    (9,6),
+                    (8,7), (9,7),
+                    (8,8),
+                    (7,9), (8,9),
+                    (7,10),
+                    (6,11), (7,11)))
+
+
 class Polyhex1234_4x10(Polyhex1234):
 
     """? (many) solutions"""
