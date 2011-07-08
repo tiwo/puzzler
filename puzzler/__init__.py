4591d7999f657395b5751eaa3cb119927b584d18
goodger <goodger@2299cb40-9015-0410-805f-d83012bcf56a>
2011-07-08 14:50:57 +0000

    updated
    git-svn-id: https://svn.code.sf.net/p/puzzler/code/trunk/puzzler@289 2299cb40-9015-0410-805f-d83012bcf56a

--- puzzler/__init__.py
+++ puzzler/__init__.py
@@ -11,7 +11,7 @@ puzzles.
 """
 
 # Author: David Goodger <goodger@python.org>
-# Copyright: (C) 1998-2010 by David J. Goodger
+# Copyright: (C) 1998-2011 by David J. Goodger
 # License: 
 #     This program is free software; you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License version 2
