063d6f09b14107b0b9e6a5794c4605b247d2d60d
goodger <goodger@2299cb40-9015-0410-805f-d83012bcf56a>
2012-05-09 03:34:53 +0000

    updated
    git-svn-id: https://svn.code.sf.net/p/puzzler/code/trunk/puzzler@513 2299cb40-9015-0410-805f-d83012bcf56a

--- bin/hexahexes-hexagon-1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# $Id$
-
-"""many solutions"""
-
-import puzzler
-from puzzler.puzzles.hexahexes import HexahexesHexagon1
-
-puzzler.run(HexahexesHexagon1)
---
+++ bin/hexahexes-hexagon-ring-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# $Id$
+
+"""many solutions"""
+
+import puzzler
+from puzzler.puzzles.hexahexes import HexahexesHexagonRing1
+
+puzzler.run(HexahexesHexagonRing1)
