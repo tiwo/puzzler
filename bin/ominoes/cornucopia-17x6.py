625a027cb989ac3fa601981c1efbcf0acaec91f6
goodger <goodger@2299cb40-9015-0410-805f-d83012bcf56a>
2013-07-03 05:43:00 +0000

    added solution count
    git-svn-id: https://svn.code.sf.net/p/puzzler/code/trunk/puzzler@585 2299cb40-9015-0410-805f-d83012bcf56a

--- bin/ominoes/cornucopia-17x6.py
+++ bin/ominoes/cornucopia-17x6.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # $Id: hexominoes-cross-1.py 571 2012-10-06 21:14:57Z goodger $
 
-"""many solutions"""
+"""162,086 solutions"""
 
 import puzzler
 from puzzler.puzzles.hexominoes import Cornucopia17x6 as puzzle
