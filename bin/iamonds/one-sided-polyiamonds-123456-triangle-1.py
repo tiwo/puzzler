2e147f532f24f58c098832726d64ddd886151f50
goodger <goodger@2299cb40-9015-0410-805f-d83012bcf56a>
2015-03-10 22:45:07 +0000

    added 10 variations on a side-13 triangle made from one-sided polyiamonds of order 1-6
    git-svn-id: https://svn.code.sf.net/p/puzzler/code/trunk/puzzler@614 2299cb40-9015-0410-805f-d83012bcf56a

---
+++ bin/iamonds/one-sided-polyiamonds-123456-triangle-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# $Id$
+
+"""many solutions"""
+
+import puzzler
+from puzzler.puzzles.polyiamonds123456 import (
+    OneSidedPolyiamonds123456Triangle1 as puzzle)
+
+puzzler.run(puzzle)
--- puzzler/puzzles/polyiamonds123456.py
+++ puzzler/puzzles/polyiamonds123456.py
@@ -38,3 +38,86 @@ class OneSidedPolyiamonds123456SemiRegularHexagon11x1(
 
     def coordinates(self):
         return self.coordinates_semiregular_hexagon(11, 1)
+
+
+class OneSidedPolyiamonds123456Triangle1(OneSidedPolyiamonds123456):
+
+    """many solutions"""
+
+    height = 13
+    width = 13
+
+    holes = set(((1,1,0), (1,10,0), (10,1,0)))
+
+    def coordinates(self):
+        coords = (
+            set(self.coordinates_triangle(self.height))
+            - self.holes)
+        return sorted(coords)
+
+    def customize_piece_data(self):
+        OneSidedPolyiamonds123456.customize_piece_data(self)
+        self.piece_data['P5'][-1]['rotations'] = (0,1)
+
+
+class OneSidedPolyiamonds123456Triangle2(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((1,1,1), (1,9,1), (9,1,1)))
+
+
+class OneSidedPolyiamonds123456Triangle3(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((2,2,0), (2,8,0), (8,2,0)))
+
+
+class OneSidedPolyiamonds123456Triangle4(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((2,2,1), (2,7,1), (7,2,1)))
+
+
+class OneSidedPolyiamonds123456Triangle5(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((3,3,0), (3,6,0), (6,3,0)))
+
+
+class OneSidedPolyiamonds123456Triangle6(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((3,3,1), (3,5,1), (5,3,1)))
+
+
+class OneSidedPolyiamonds123456Triangle7(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((0,6,0), (6,0,0), (6,6,0)))
+
+
+class OneSidedPolyiamonds123456Triangle8(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((1,5,1), (5,1,1), (5,5,1)))
+
+
+class OneSidedPolyiamonds123456Triangle9(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((2,5,0), (5,2,0), (5,5,0)))
+
+
+class OneSidedPolyiamonds123456Triangle10(OneSidedPolyiamonds123456Triangle1):
+
+    """many solutions"""
+
+    holes = set(((3,4,1), (4,3,1), (4,4,1)))
